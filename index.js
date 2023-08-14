172839e9acfb3c4aabf55eb2e02583e93c55397b
Elmer Mamani <elmer.mamani.urrelo@gmail.com>
2023-08-14 10:05:54 -0400

    Ejercicio terminado.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-// reference: https://www.npmjs.com/package/dotenv
+const fs = require('node:fs')
 
 function config (options = {}) {
-  // your code here
+  try{
+    const path = options.path  ?? '.env'
+    const envFile = fs.readFileSync(path, 'utf-8')
+    if(envFile === '') return
+
+    const envData = envFile.replaceAll(/[\r\"\']/g,'').split('\n');
+    const keyList = envData.map(key => {
+      return [ key.split('=')[0],  key.split('=')[1] ]
+    });
+
+    process.env = Object.fromEntries(keyList);
+  }catch(err){
+    if(err.code === 'ENOENT') return;
+  }
 }
 
 module.exports = { config }
